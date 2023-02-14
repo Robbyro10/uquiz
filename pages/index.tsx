7c9a7582f6ded13b62f94a0bfefb34e96fe41959
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2023-02-14 16:05:09 -0400

    better styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,11 @@ export default function Home() {
       return;
     }
     Swal.fire({
-      title: 'Oopsies!',
-      text: 'Please enter your name',
-      icon: 'error',
-      confirmButtonColor: 'rgb(244 114 182)'
-    })
+      title: "Oopsies!",
+      text: "Please enter your name",
+      icon: "error",
+      confirmButtonColor: "rgb(244 114 182)",
+    });
   };
 
   return (
@@ -32,31 +32,35 @@ export default function Home() {
       pageDescription="With this test
     you will determine your next crime"
     >
-      <Title body="Which will be your next crime?" />
-      <h2 className="text-center mb-4 text-sm">
-        Hey folk&apos;s Mr. Hedderich over here. I made this incredibly aesthetic
-        quiz with love and passion. With this
-        test you will determine your next crime <b>(100% CERTIFIED)</b>.{" "}
-        <i>Results will not be saved or registered in any way.</i>
-      </h2>
+      <div className="flex justify-center">
+        <div className="flex flex-col w-2/3">
+          <Title body="Which will be your next crime?" />
+          <h2 className="text-center mb-4 text-sm">
+            Hey folk&apos;s Mr. Hedderich over here. I made this incredibly
+            aesthetic quiz with love and passion. With this test you will
+            determine your next crime <b>(100% CERTIFIED)</b>.{" "}
+            <i>Results will not be saved or registered in any way.</i>
+          </h2>
+        </div>
+      </div>
 
       <div className="text-center">
         <h2 className="font-semibold text-lg">Enter your name</h2>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className="rounded px-2 py-1 my-4"
-          ></input>
-          <br />
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="bg-pink-400 hover:bg-pink-300 text-white py-1 px-8 rounded text-lg transition ease-linear drop-shadow animate__animated animate__fadeIn"
-          >
-            Start
-          </button>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className="rounded px-2 py-1 my-4"
+        ></input>
+        <br />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-pink-400 hover:bg-pink-300 font-bold text-white py-1 px-8 rounded text-lg transition ease-linear drop-shadow animate__animated animate__fadeIn"
+        >
+          Start
+        </button>
       </div>
       <div className="flex justify-center mt-48">
         <Image
--- pages/quiz/results.tsx
+++ pages/quiz/results.tsx
@@ -8,8 +8,8 @@ import { useContext } from "react";
 
 const ResultsPage = () => {
   const router = useRouter();
-  const {score, name} = useContext(QuizContext)
-  const result = calculateResult(score)
+  const { score, name } = useContext(QuizContext);
+  const result = calculateResult(score);
 
   return (
     <QuizLayout
@@ -17,19 +17,29 @@ const ResultsPage = () => {
       pageDescription="The results of your test arrived"
     >
       <div className="text-center">
-      <h2>Congratulations <b>{name}</b>!! you will commit:</h2>
-      <Title body={result.title} />
-      <p>{result.desc}</p>
-
+        <h2>
+          Congratulations <b>{name}</b>!! you will commit:
+        </h2>
+        <Title body={result.title} />
+        <p>{result.desc}</p>
       </div>
-      <Image src={result.img} alt={result.title} className="my-10 rounded drop-shadow"/>
-      
+      <div className="flex justify-center">
+      <div className="flex flex-col mt-10">
+        <Image
+          src={result.img}
+          alt={result.title}
+          className="my-10 rounded drop-shadow"
+        />
       <button
-        className=" bg-pink-400 hover:bg-pink-300 rounded py-2 px-4 text-white transition ease-linear drop-shadow "
+        className="bg-pink-400 hover:bg-pink-300 rounded py-4 px-4 font-bold text-white transition ease-linear drop-shadow "
         onClick={() => router.push("/")}
       >
         Retake Test
       </button>
+      </div>
+
+      </div>
+
     </QuizLayout>
   );
 };
