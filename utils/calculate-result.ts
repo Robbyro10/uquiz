3566cd5f7625be22660647763b16a766fb3d9dda
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2023-02-15 11:06:14 -0400

    fixed scoring

--- utils/calculate-result.ts
+++ utils/calculate-result.ts
@@ -2,31 +2,31 @@ import { crimes } from "@/data";
 
 export const calculateResult = (score: number) => {
 
-  if (score > 45) {
+  if (score > 33) {
     return crimes[9];
   }
-  else if (score > 40) {
+  else if (score > 32) {
     return crimes[8]
   }
-  else if (score > 35) {
+  else if (score > 31) {
     return crimes[7]
   }
   else if (score > 30) {
     return crimes[6]
   }
-  else if (score > 25) {
+  else if (score > 28) {
     return crimes[5]
   }
-  else if (score > 20) {
+  else if (score > 26) {
     return crimes[4]
   }
-  else if (score > 15) {
+  else if (score > 25) {
     return crimes[3]
   }
-  else if (score > 10) {
+  else if (score > 24) {
     return crimes[2]
   }
-  else if (score > 5) {
+  else if (score > 23) {
     return crimes[1]
   }
   else {
